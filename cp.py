529cd6f2c0923221c3fb9891380bea766d73a9e9
iitzex <iitzex@gmail.com>
2019-04-23 20:07:01 +0800

    add cp.py

---
+++ cp.py
@@ -0,0 +1,18 @@
+import os
+from shutil import copyfile
+
+
+src_p = '/Volumes/GARMIN/GARMIN/ACTIVITY/'
+dst_p = 'FIT/'
+
+
+def main():
+    src = set(os.listdir(src_p))
+    dst = set(os.listdir(dst_p))
+
+    for i in src.difference(dst):
+        copyfile(f'{src_p}{i}', f'{dst_p}{i}')
+
+
+if __name__ == "__main__":
+    main()
