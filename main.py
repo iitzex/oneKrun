396d6036158cac6ef5f07abc66f2f6d32332fbab
iitzex <iitzex@gmail.com>
2019-04-27 16:49:34 +0800

    add single track

--- main.py
+++ main.py
@@ -8,14 +8,14 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from bokeh.embed import components
 from bokeh.io import export_png, export_svgs
-from bokeh.layouts import gridplot
+from bokeh.layouts import gridplot, column
 from bokeh.models import ColumnDataSource
 from bokeh.palettes import all_palettes
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, output_file, show, save
 from fitparse import FitFile
 
 palettes = all_palettes['Viridis'][5]
-COLORS = {'text': '#222222', 'bg': '#FFFFFF', 'special': '#FFFF00',
+COLORS = {'text': '#222222', 'bg': '#FFFFFF', 'special': '#FFD700',
           '42k': palettes[4], '21k': palettes[3], '10k': palettes[2], '4k': palettes[1], '1k': palettes[0]}
 NCOLS = 7
 SIZE = 150
@@ -37,12 +37,12 @@ def get_color(distance):
 
 def fig(fn):
     fitfile = FitFile(fn)
-    total = []
+    table = []
     for record in fitfile.get_messages('record'):
         values = record.get_values()
-        total.append(values)
+        table.append(values)
 
-    df = pd.DataFrame(total)
+    df = pd.DataFrame(table)
 
     try:
         df['lat'] = df.position_lat.map(lambda x: x * 180 / (2 << 30))
@@ -56,33 +56,41 @@ def fig(fn):
     # msg = f"{df.iloc[-1]['timestamp']}  {df.iloc[-1]['distance']}"
     distance = df.iloc[-1]['distance'] / 1000
     color = get_color(distance)
-
-    # print(df.long)
-    # df.plot(kind='line', x='long', y='lat', c='distance', colormap='viridis')
-    # plt.scatter(df['long'], df['lat'], c=df['distance'], s=1)
-    # plt.plot(df['long'], df['lat'], c='c')
-    # plt.show()
-
+    timestamp = df.iloc[-1]['timestamp']
     source = ColumnDataSource(df)
-    p = figure(plot_width=SIZE, plot_height=SIZE+10,
-               toolbar_location=None, tools="", title=str(distance))
-    # p.circle(df['long'], df['lat'], size=1)
-    # p.circle("long", "lat", size=5, source=source)
-    p.line('long', 'lat', color=color, line_width=3, source=source)
 
-    # p.background_fill_color = COLORS['bg']
-    p.outline_line_color = COLORS['bg']
-    p.min_border_left = BORDER
-    p.min_border_right = BORDER
-    p.min_border_top = BORDER
-    p.min_border_bottom = BORDER
-    p.grid.grid_line_color = None
-    p.axis.visible = False
-
-    export_png(p, filename=f'dup/img/{fn[4:-4]}.png')
+    track = figure(plot_width=SIZE, plot_height=SIZE+10,
+                   toolbar_location=None, tools="", title=str(distance))
+    track.line('long', 'lat', color=color, line_width=3, source=source)
+    track.outline_line_color = COLORS['bg']
+    track.min_border_left = BORDER
+    track.min_border_right = BORDER
+    track.min_border_top = BORDER
+    track.min_border_bottom = BORDER
+    track.grid.grid_line_color = None
+    track.axis.visible = False
+    export_png(track, filename=f'dup/img/{fn[4:-4]}.png')
     # p.output_backend = "svg"
     # export_svgs(p, filename=f'dup/img/{fn[4:-4]}.svg')
-    return p, distance
+
+    single = figure(plot_width=SIZE*4, plot_height=SIZE*4+10, title=str(distance),
+                    toolbar_location=None, tools="")
+    single.line('long', 'lat', color=color, line_width=3, source=source)
+    hr = figure(plot_width=SIZE*4, plot_height=int(SIZE*1.3), title='HEART_RATE',
+                toolbar_location=None, tools="")
+    hr.line('distance', 'heart_rate', line_width=3, source=source)
+    speed = figure(plot_width=SIZE*4, plot_height=int(SIZE*1.3), title='SPEED',
+                   toolbar_location=None, tools="")
+    speed.line('distance', 'cadence', line_width=3, source=source)
+    cadence = figure(plot_width=SIZE*4, plot_height=int(SIZE*1.3), title='CADENCE',
+                     toolbar_location=None, tools="", )
+    cadence.line('distance', 'cadence', line_width=3, source=source)
+    plot = column([single, hr, speed, cadence])
+    # export_png(plot, filename=f'dup/track/{fn[4:-4]}.png')
+    save(plot, f'dup/track/{fn[4:-4]}.html',
+         title=timestamp.strftime('%Y/%m/%d'))
+
+    return track, distance
 
 
 def main():
@@ -99,18 +107,16 @@ def main():
             p, distance = fig(path)
             figs.append(p)
             total += distance
-            # img_html += f'<div style="width:10%;height:10%;"><div class="single-gallery-image" style="background:url(img/{img_name}.svg);"></div></div>\n'
             img_html += f"<p><img src='img/{img_name}.png' width='100%'/></p>\n"
         except TypeError:
             pass
 
-    print(img_html)
     plot = gridplot(figs, ncols=NCOLS, toolbar_location=None)
-    # export_png(plot, filename=f'oneKrun.png')
+    export_png(plot, filename=f'oneKrun.png')
     # output_file('run.html')
     # show(p)
-
     # script, div = components(plot)
+
     script = img_html
     div = ''
 
